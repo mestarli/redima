8d1b75569a170383d16dd85d26d2c48cf06746ce
mestarli <97691723+mestarli@users.noreply.github.com>
2022-05-06 18:59:19 +0200

    Added scripts for movement, attack, player

--- Assets/Scripts/Mary/PlayerAttack.cs
+++ Assets/Scripts/Mary/PlayerAttack.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
+    
     // Start is called before the first frame update
     void Start()
     {
--- Assets/Scripts/Mary/PlayerMovement.cs
+++ Assets/Scripts/Mary/PlayerMovement.cs
@@ -8,14 +8,15 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float life = 100f;
     [SerializeField] private float maxLife = 100f;
     [SerializeField] private float gravity = -9.81f;
-    [SerializeField] private float speed = 8.5f;
-    [SerializeField] private float speedRun = 15.5f;
+    [SerializeField] private float speed = 1.5f;
+    [SerializeField] private float speedRun = 2.5f;
     private float initialSpeed;
     private CharacterController _characterController;
     
     [SerializeField] private bool isGrounded;
     [SerializeField] private bool isJumping;
     [SerializeField] private float jumHeight = 3.5f;
+    private Vector3 playerVelocity;
 
     private Animator _animator;
     
@@ -58,15 +59,34 @@ public class PlayerMovement : MonoBehaviour
     
     private void Movement()
     {
+        isGrounded =  _characterController.isGrounded;
+        if (isGrounded && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
         inputPlayerMovement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-  
+        _characterController.Move(  inputPlayerMovement * Time.deltaTime * speed);
+
+        if (inputPlayerMovement != Vector3.zero)
+        {
+            gameObject.transform.forward = inputPlayerMovement;
+        }
+
+        // Changes the height position of the player..
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumHeight * -3.0f * gravity);
+        }
+
+        playerVelocity.y += gravity * Time.deltaTime;
+        _characterController.Move(playerVelocity * Time.deltaTime);
         Run();
     }
     private void Jump()
     {
         if(Input.GetKeyDown(KeyCode.Space) && isGrounded){
             
-            //_rigidbody.AddForce(jumHeight * Vector3.up, ForceMode.VelocityChange);
             //_animator.SetTrigger("IsJumping");
         }
 
