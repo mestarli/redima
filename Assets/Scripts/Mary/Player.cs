e7fe07b9cc0117af62c2fdefbfe0403bf7ac60f8
JArbat9803 <j.arbat@santmarc.com>
2022-05-13 17:29:35 +0200

    Update Player hambre y estamina

--- Assets/Scripts/Mary/Player.cs
+++ Assets/Scripts/Mary/Player.cs
@@ -1,9 +1,23 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float hambre;
+    private float maxHambre;
+    
+    public float estamina;
+    private float maxEstamina;
+
+    private float diferencia;
+    private void Awake()
+    {
+        estamina = maxEstamina;
+        hambre = maxHambre;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +28,45 @@ public class Player : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void alimentarse(float cantidad)
+    {
+        diferencia = maxHambre - hambre;
+        if (hambre < maxHambre)
+        {
+            if (cantidad < diferencia)
+            {
+                hambre += hambre + cantidad;
+            }
+            else
+            {
+                hambre += hambre + diferencia; 
+            }
+        }
+        else
+        {
+            return;
+        }
+    }
+    
+    public void recuperarEstamina(float cantidad)
+    {
+        diferencia = maxEstamina - estamina;
+        if (estamina < maxEstamina)
+        {
+            if (cantidad < diferencia)
+            {
+                estamina += estamina + cantidad;
+            }
+            else
+            {
+                estamina += estamina + diferencia; 
+            }
+        }
+        else
+        {
+            return;
+        }
     }
 }
