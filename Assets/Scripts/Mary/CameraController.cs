b204442c1c9af0043c799c053eb7ae63f97d9584
mestarli <97691723+mestarli@users.noreply.github.com>
2022-05-25 01:14:15 +0200

    Update cameras

--- Assets/Scripts/Mary/CameraController.cs
+++ Assets/Scripts/Mary/CameraController.cs
@@ -6,22 +6,31 @@ public class CameraController : MonoBehaviour
 {
     [SerializeField] private CinemachineVirtualCamera camera_01;
     [SerializeField] private CinemachineVirtualCamera camera_02;
+    [SerializeField] private CinemachineVirtualCamera camera_03;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && gameObject.tag == "Activate_Camera_02")
         {
             if (camera_01.Priority > camera_02.Priority )
             {
                 camera_01.Priority = camera_01.Priority - 1;
                 camera_02.Priority = camera_02.Priority + 1;
+                camera_03.Priority = 9;
             }
             else
             {
                 camera_01.Priority = camera_01.Priority + 1;
                 camera_02.Priority = camera_02.Priority - 1;
+                camera_03.Priority = 9;
             }
 
+        }
+        else
+        {
+            camera_01.Priority = camera_01.Priority - 1;
+            camera_02.Priority = camera_02.Priority - 1;
+            camera_03.Priority = 12;
         }
     }
 }
