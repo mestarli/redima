602592fbdab0af7e412461d361e0461365c0b109
mestarli <97691723+mestarli@users.noreply.github.com>
2022-05-26 00:51:58 +0200

    Update camera setting

--- Assets/Scripts/Mary/CameraController.cs
+++ Assets/Scripts/Mary/CameraController.cs
@@ -5,12 +5,81 @@ using Cinemachine;
 public class CameraController : MonoBehaviour
 {
     [SerializeField] private List<CinemachineVirtualCamera> cameras;
-
+    [SerializeField] private CinemachineVirtualCamera activeCamera;
+    [SerializeField] private string tagTrigger;
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-        
+            tagTrigger = gameObject.tag;
+            Debug.Log("Tag "+tagTrigger);
+            foreach (var camera in cameras)
+            {
+                Debug.Log("Tag de la camara "+camera.tag);
+                if (camera.tag.Contains(tagTrigger) && camera.tag != tagTrigger)
+                {
+                    if (camera.Priority == 10)
+                    {
+                        camera.Priority = 12;
+                        continue;
+                    }
+                    if (camera.Priority == 12)
+                    {
+                        camera.Priority = 10;
+                        continue;
+                    }
+                }
+                if (camera.tag.Contains(tagTrigger) && camera.tag == tagTrigger)
+                {
+                    if (camera.Priority == 10)
+                    {
+                        camera.Priority = 12;
+                        continue;
+                    }
+                    if (camera.Priority == 12)
+                    {
+                        camera.Priority = 10;
+                        continue;
+                    }
+                }
+            }
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            tagTrigger = gameObject.tag;
+            Debug.Log("Tag Exit"+tagTrigger);
+            foreach (var camera in cameras)
+            {
+                if (camera.tag.Contains(tagTrigger) && camera.tag != tagTrigger)
+                {
+                    if (camera.Priority == 10)
+                    {
+                        camera.Priority = 12;
+                        continue;
+                    }
+                    if (camera.Priority == 12)
+                    {
+                        camera.Priority = 10;
+                        continue;
+                    }
+                }
+                if (camera.tag.Contains(tagTrigger) && camera.tag == tagTrigger)
+                {
+                    if (camera.Priority == 10)
+                    {
+                        camera.Priority = 12;
+                        continue;
+                    }
+                    if (camera.Priority == 12)
+                    {
+                        camera.Priority = 10;
+                        continue;
+                    }
+                }
+            }
         }
     }
 }
