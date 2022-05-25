8b72a86687cd3c055dcaf896011fc6e2d174e097
mestarli <97691723+mestarli@users.noreply.github.com>
2022-05-25 23:29:41 +0200

    added cameras

--- Assets/Scripts/Mary/CameraController.cs
+++ Assets/Scripts/Mary/CameraController.cs
@@ -4,33 +4,13 @@ using UnityEngine;
 using Cinemachine;
 public class CameraController : MonoBehaviour
 {
-    [SerializeField] private CinemachineVirtualCamera camera_01;
-    [SerializeField] private CinemachineVirtualCamera camera_02;
-    [SerializeField] private CinemachineVirtualCamera camera_03;
+    [SerializeField] private List<CinemachineVirtualCamera> cameras;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player") && gameObject.tag == "Activate_Camera_02")
+        if (other.CompareTag("Player"))
         {
-            if (camera_01.Priority > camera_02.Priority )
-            {
-                camera_01.Priority = camera_01.Priority - 1;
-                camera_02.Priority = camera_02.Priority + 1;
-                camera_03.Priority = 9;
-            }
-            else
-            {
-                camera_01.Priority = camera_01.Priority + 1;
-                camera_02.Priority = camera_02.Priority - 1;
-                camera_03.Priority = 9;
-            }
-
-        }
-        else
-        {
-            camera_01.Priority = camera_01.Priority - 1;
-            camera_02.Priority = camera_02.Priority - 1;
-            camera_03.Priority = 12;
+        
         }
     }
 }
