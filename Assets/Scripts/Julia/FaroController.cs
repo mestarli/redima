00fa85bde3fb77302e2e747c3fde31ff051e8548
JArbat9803 <97691717+JArbat9803@users.noreply.github.com>
2022-06-05 19:06:26 +0200

    LuzFaro

---
+++ Assets/Scripts/Julia/FaroController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaroController : MonoBehaviour
+{
+    // Variables
+    public GameObject faro;
+    public float angleRotation;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateFaro();
+    }
+
+    public void UpdateFaro()
+    {
+      faro.transform.Rotate(0, angleRotation * Time.deltaTime, 0);
+    }
+}
