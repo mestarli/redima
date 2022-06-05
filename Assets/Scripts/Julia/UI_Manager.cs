4a30c7612309214e777b0c6689d6ec56fbbd0609
JArbat9803 <97691717+JArbat9803@users.noreply.github.com>
2022-06-05 21:01:15 +0200

    Botones Abrir Paneles check

--- Assets/Scripts/Julia/UI_Manager.cs
+++ Assets/Scripts/Julia/UI_Manager.cs
@@ -9,10 +9,15 @@ public class UI_Manager : MonoBehaviour, ITimeTracker
 {
     // Variables
     [Header("PANELS")]
-    [SerializeField] private GameObject inGamePanel;
+    [SerializeField] private GameObject mapPanel;
+    [SerializeField] private GameObject pecedexPanel;
     [SerializeField] private GameObject optionsPanel;
+    [SerializeField] private GameObject animadexPanel;
     [SerializeField] private GameObject mainMenuPanel;
+    [SerializeField] private GameObject plantadexPanel;
+    [SerializeField] private GameObject recetadexPanel;
     [SerializeField] private GameObject inventoryPanel;
+    [SerializeField] private GameObject enciclopediaPanels;
 
     [Header("Status Panel")]
     private List<HandSlot> availableSlot = new List<HandSlot>();
@@ -31,39 +36,96 @@ public class UI_Manager : MonoBehaviour, ITimeTracker
         if (instance == null)
         {
             instance = this;
-            //DontDestroyOnLoad(gameObject);
         }
 
         else
         {
             Destroy(gameObject);
         }
-        try {
-            inGamePanel.SetActive(false);
-        }       
-        catch (UnassignedReferenceException ex) {
-            Debug.Log("In Game Panel not assigned");
+
+        #region TryCatch
+
+        try
+        {
+            mapPanel.SetActive(false);
         }
-      
-        try {
-            optionsPanel.SetActive(false);
-        }       
-        catch (UnassignedReferenceException ex) {
-            Debug.Log("Options Panel not assigned");
-        }
-        try {
-            mainMenuPanel.SetActive(true);
-        }       
-        catch (UnassignedReferenceException ex) {
-            Debug.Log("Main Menu Panel not assigned");
-        }
-        try {
-            inventoryPanel.SetActive(false);
-        }       
-        catch (UnassignedReferenceException ex) {
-            Debug.Log("Inventory Panel not assigned");
+        catch (UnassignedReferenceException ex)
+        {
+            Debug.Log("Map Panel not assigned");
         }
 
+        try
+        {
+            pecedexPanel.SetActive(false);
+        }
+        catch (UnassignedReferenceException ex)
+        {
+            Debug.Log("Pecedex Panel not assigned");
+        }
+      
+        try 
+        {
+            optionsPanel.SetActive(false);
+        }
+        catch (UnassignedReferenceException ex) 
+        {
+            Debug.Log("Options Panel not assigned");
+        }
+        
+        try 
+        {
+            animadexPanel.SetActive(false);
+        }
+        catch (UnassignedReferenceException ex) 
+        {
+            Debug.Log("Animadex Panel not assigned");
+        }
+        
+        try 
+        {
+            mainMenuPanel.SetActive(true);
+        }
+        catch (UnassignedReferenceException ex) 
+        {
+            Debug.Log("Main Menu Panel not assigned");
+        }
+        
+        try 
+        {
+            plantadexPanel.SetActive(false);
+        }
+        catch (UnassignedReferenceException ex) 
+        {
+            Debug.Log("Plantadex Panel not assigned");
+        }
+        
+        try 
+        {
+            recetadexPanel.SetActive(false);
+        }
+        catch (UnassignedReferenceException ex) 
+        {
+            Debug.Log("Recetadex Panel not assigned");
+        }
+        
+        try 
+        {
+            inventoryPanel.SetActive(false);
+        }
+        catch (UnassignedReferenceException ex) 
+        {
+            Debug.Log("Inventory Panel not assigned");
+        }
+        
+        try 
+        {
+            enciclopediaPanels.SetActive(false);
+        }
+        catch (UnassignedReferenceException ex) 
+        {
+            Debug.Log("Enciclopedia Panel not assigned");
+        }
+        #endregion
     }
 
     private void Start()
@@ -113,18 +175,28 @@ public class UI_Manager : MonoBehaviour, ITimeTracker
     {
         SceneManager.LoadScene("MainScene");
         
-        inGamePanel.SetActive(true);
+        mapPanel.SetActive(false);
+        pecedexPanel.SetActive(false);
         optionsPanel.SetActive(false);
+        animadexPanel.SetActive(false);
         mainMenuPanel.SetActive(false);
+        plantadexPanel.SetActive(false);
+        recetadexPanel.SetActive(false);
         inventoryPanel.SetActive(false);
+        enciclopediaPanels.SetActive(false);
     }
 
     public void Options()
     {
-        inGamePanel.SetActive(false);
+        mapPanel.SetActive(false);
+        pecedexPanel.SetActive(false);
         optionsPanel.SetActive(true);
+        animadexPanel.SetActive(false);
         mainMenuPanel.SetActive(false);
+        plantadexPanel.SetActive(false);
+        recetadexPanel.SetActive(false);
         inventoryPanel.SetActive(false);
+        enciclopediaPanels.SetActive(true);
     }
     
     public void QuitGame()
@@ -136,8 +208,81 @@ public class UI_Manager : MonoBehaviour, ITimeTracker
 
     public void OpenCloseInventoryPanel()
     {
+        enciclopediaPanels.SetActive(!enciclopediaPanels.activeSelf);
         inventoryPanel.SetActive(!inventoryPanel.activeSelf);
     }
+    
+    public void OpenCloseMapPanel()
+    {
+        enciclopediaPanels.SetActive(!enciclopediaPanels.activeSelf);
+        mapPanel.SetActive(!mapPanel.activeSelf);
+    }
+    
+    public void PassToMapPanel()
+    {
+        mapPanel.SetActive(true);
+        pecedexPanel.SetActive(false);
+        optionsPanel.SetActive(false);
+        animadexPanel.SetActive(false);
+        plantadexPanel.SetActive(false);
+        recetadexPanel.SetActive(false);
+        inventoryPanel.SetActive(false);
+    }
+    
+    public void PassToPecedexPanel()
+    {
+        mapPanel.SetActive(false);
+        pecedexPanel.SetActive(true);
+        optionsPanel.SetActive(false);
+        animadexPanel.SetActive(false);
+        plantadexPanel.SetActive(false);
+        recetadexPanel.SetActive(false);
+        inventoryPanel.SetActive(false);
+    }
+    
+    public void PassToAnimadexPanel()
+    {
+        mapPanel.SetActive(false);
+        pecedexPanel.SetActive(false);
+        optionsPanel.SetActive(false);
+        animadexPanel.SetActive(true);
+        plantadexPanel.SetActive(false);
+        recetadexPanel.SetActive(false);
+        inventoryPanel.SetActive(false);
+    }
+
+    public void PassToPlantadexPanel()
+    {
+        mapPanel.SetActive(false);
+        pecedexPanel.SetActive(false);
+        optionsPanel.SetActive(false);
+        animadexPanel.SetActive(false);
+        plantadexPanel.SetActive(true);
+        recetadexPanel.SetActive(false);
+        inventoryPanel.SetActive(false);
+    }
+
+    public void PassToRecetadexPanel()
+    {
+        mapPanel.SetActive(false);
+        pecedexPanel.SetActive(false);
+        optionsPanel.SetActive(false);
+        animadexPanel.SetActive(false);
+        plantadexPanel.SetActive(false);
+        recetadexPanel.SetActive(true);
+        inventoryPanel.SetActive(false);
+    }
+    
+    public void PassToInventoryPanel()
+    {
+        mapPanel.SetActive(false);
+        pecedexPanel.SetActive(false);
+        optionsPanel.SetActive(false);
+        animadexPanel.SetActive(false);
+        plantadexPanel.SetActive(false);
+        recetadexPanel.SetActive(false);
+        inventoryPanel.SetActive(true);
+    } 
 
     #endregion
 }
