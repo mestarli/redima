1cb0b97cbd9ef1afe22cc67f3bef3087695d32e8
mestarli <97691723+mestarli@users.noreply.github.com>
2022-06-09 10:44:06 +0200

    Recetadex complete

--- Assets/Scripts/Mary/Recetadex.cs
+++ Assets/Scripts/Mary/Recetadex.cs
@@ -21,6 +21,7 @@ public class Recetadex : MonoBehaviour
     {
         if (gameObject.transform.GetChild(1).gameObject.tag == "Active")
         {
+            item_pokedex.gameObject.SetActive(true);
             item_pokedex.sprite = image_pokedex.sprite;
             title_pokedex.text = title_pokedex_txt;
             text_pokedex.text = text_pokedex_txt;
