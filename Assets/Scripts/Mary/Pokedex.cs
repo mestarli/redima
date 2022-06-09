29caeb2c62c26792a2fd1752d83322370b38abb7
mestarli <97691723+mestarli@users.noreply.github.com>
2022-06-09 11:11:37 +0200

    update pa thiago

--- Assets/Scripts/Mary/Pokedex.cs
+++ Assets/Scripts/Mary/Pokedex.cs
@@ -2,22 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+
 public class Pokedex : MonoBehaviour
 {
 
-    [SerializeField] private GameObject item_pokedex_object;
-    
     [SerializeField] private Image image_pokedex;
 
     [SerializeField] private string title_pokedex_txt;
-    
+
     [SerializeField] private Text title_pokedex;
-    
+
     [SerializeField] private string text_pokedex_txt;
-    
+
     [SerializeField] private Text text_pokedex;
 
     [SerializeField] private Image item_pokedex;
+
     // Start is called before the first frame update
     public void showInfo()
     {
@@ -27,5 +27,6 @@ public class Pokedex : MonoBehaviour
             item_pokedex.sprite = image_pokedex.sprite;
             title_pokedex.text = title_pokedex_txt;
             text_pokedex.text = text_pokedex_txt;
-        }   
+        }
     }
+}    
