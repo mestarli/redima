e727cc9a89f5defc60703aa678c1c367f988aac4
mestarli <97691723+mestarli@users.noreply.github.com>
2022-06-09 10:53:03 +0200

    Update Pokedex.cs

--- Assets/Scripts/Mary/Pokedex.cs
+++ Assets/Scripts/Mary/Pokedex.cs
@@ -1,23 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Pokedex : MonoBehaviour
 {
-    [SerializeField] private GameObject image_pokedex;
 
-    [SerializeField] private GameObject text_pokedex;
+    [SerializeField] private GameObject item_pokedex_object;
+    
+    [SerializeField] private Image image_pokedex;
 
-    [SerializeField] private GameObject item_pokedex;
+    [SerializeField] private string title_pokedex_txt;
+    
+    [SerializeField] private Text title_pokedex;
+    
+    [SerializeField] private string text_pokedex_txt;
+    
+    [SerializeField] private Text text_pokedex;
+
+    [SerializeField] private Image item_pokedex;
     // Start is called before the first frame update
-    void Start()
+    public void showInfo()
     {
-        
+        if (gameObject.transform.GetChild(1).gameObject.tag == "Active")
+        {
+            item_pokedex.gameObject.SetActive(true);
+            item_pokedex.sprite = image_pokedex.sprite;
+            title_pokedex.text = title_pokedex_txt;
+            text_pokedex.text = text_pokedex_txt;
+        }   
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
